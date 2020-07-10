72b99b72a5b273b0952ec2aea5ba1e6725430a6b
Emma Finkel <emma.finkel@gmail.com>
2020-07-10 09:29:57 -0400

    Initial Commit

---
+++ Camera&DisplayPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera&DisplayPhoto
+//
+//  Created by Emma Finkel on 7/10/20.
+//  Copyright © 2020 Emma Finkel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
